00b758fe68febe0c9bda10071d924c6fdc012df8
Abramov Anton <abramovantonru@ya.ru>
2017-10-11 19:11:56 +0300

    init project

---
+++ upload.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<form enctype="multipart/form-data" method="post" action="">
+	<div>
+		<div><label for="image_file">Please select image file</label></div>
+		<div><input type="file" name="image_file" id="image_file" /></div>
+	</div>
+	<div>
+		<input type="submit" value="Загрузить" />
+	</div>
+</form>
