f3b73f93d5e93c94bf3645d352215157aa6cc032
Abramov Anton <abramovantonru@ya.ru>
2017-10-16 19:48:46 +0300

    update upload form

--- upload.php
+++ upload.php
@@ -1,12 +1,30 @@
-<?php
+<?
 
 ?>
-<form enctype="multipart/form-data" method="post" action="">
-	<div>
-		<div><label for="image_file">Please select image file</label></div>
-		<div><input type="file" name="image_file" id="image_file" /></div>
-	</div>
-	<div>
-		<input type="submit" value="Загрузить" />
-	</div>
-</form>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+
+<div class="wrapper">
+	<form enctype="multipart/form-data" method="post" action="">
+		<div>
+			<div>
+				<label for="image_file">Выберите файл с логом</label>
+			</div>
+			<div>
+				<input type="file" name="image_file" id="image_file"/>
+			</div>
+		</div>
+		<div>
+			<input type="submit" value="Загрузить" class="upload-btn"/>
+		</div>
+	</form>
+</div>
+
+</body>
+</html>
